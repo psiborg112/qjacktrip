d7a2ae6ca3c8f4ebe1f98cc0435df5e324a6c0eb
Aaron Wyatt <github@psi-borg.org>
2024-12-30 10:47:14 +1100

    Update package post install script

--- macos/package/postinstall.sh
+++ macos/package/postinstall.sh
@@ -3,7 +3,8 @@
 # Link jacktrip to app binary
 mkdir -p /usr/local/bin
 rm -f /usr/local/bin/jacktrip
-ln -s "$2"/Contents/MacOS/jacktrip /usr/local/bin/jacktrip
+ln -s "$2"/Contents/MacOS/qjacktrip /usr/local/bin/qjacktrip
+[ ! -f "/usr/local/bin/jacktrip" ] && ln -s qjacktrip /usr/local/bin/jacktrip
 
 # Open JackTrip on intaller finish
 #open -a /Applications/JackTrip.app
